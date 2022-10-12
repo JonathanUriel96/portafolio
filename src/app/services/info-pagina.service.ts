930a607140aaaa2d9d12f293616579a90bb23c93
JonathanUriel96 <49410544+JonathanUriel96@users.noreply.github.com>
2022-10-11 23:40:00 -0500

    Section 5 Ended

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,34 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
+    this.cargarInfo();
+    this.cargarEquipo();
     //console.log('Servicio de infoPagina listo');
+
+   }
+
+   private cargarInfo() {
 
     // Leer el archivo JSON
     this.http.get('assets/data/data-page.json')
-        .subscribe( ( resp: InfoPagina ) => {
+    .subscribe( ( resp: InfoPagina ) => {
+      this.cargada = true;
+      this.info = resp;
+    });
+   }
 
-          this.cargada = true;
-          this.info = resp;
+   private cargarEquipo() {
+
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-aa801-default-rtdb.firebaseio.com/equipo.json')
+        .subscribe( ( resp: any ) => {
+
+          this.equipo = resp;
           console.log(resp);
-
         });
-
    }
 }
